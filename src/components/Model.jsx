c7ecc37601f0e3530afc5335e1a5a6b1531ec50b
DenRisk <denisniklas.risken@sybit.de>
2025-05-14 15:48:48 +0200

    Second commit

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,21 +1,18 @@
 import { useGLTF, useTexture } from '@react-three/drei'
-import React from 'react'
 import * as THREE from 'three'
 
 export function Model() {
-  const { scene } = useGLTF('/models/Roomv7.glb')
-  const texture01 = useTexture('/textures/TexturePack01Baked.jpg')
-  const texture02 = useTexture('/textures/TexturePack02Baked.jpg')
-  const texture03 = useTexture('/textures/TexturePack03Baked.jpg')
+  const { scene } = useGLTF('/models/Roomv11.glb')
+  const texture01 = useTexture('/textures/TexturePack01.jpg')
+  const texture02 = useTexture('/textures/TexturePack02.jpg')
+  const texture03 = useTexture('/textures/TexturePack03.jpg')
 
   // Optional: sRGB-Encoding für realistischere Farben
-  texture01.encoding = THREE.sRGBEncoding
+  texture01.colorSpace = THREE.SRGBColorSpace
+  texture02.colorSpace = THREE.SRGBColorSpace
+  texture03.colorSpace = THREE.SRGBColorSpace
   texture01.flipY = false
-
-  texture02.encoding = THREE.sRGBEncoding
   texture02.flipY = false
-
-  texture03.encoding = THREE.sRGBEncoding
   texture03.flipY = false
 
   // Setze das bakedMaterial für alle Meshes
